e769161e3a8483fee7471c7c3ea8776e0241b1bd
apurvaNpradhan <apurvanarayanpradhan@gmail.com>
2025-03-23 16:18:43 +0530

    feat: blog setup

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,20 @@
+import { CustomMDX } from "@/components/mdx";
+import { getBlogPosts } from "@/utils/blog";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  let post = getBlogPosts().find((post) => post.slug === slug);
+
+  return (
+    <div>
+      {post?.metadata.title}
+      <article>
+        <CustomMDX source={post?.content} />
+      </article>
+    </div>
+  );
+}
